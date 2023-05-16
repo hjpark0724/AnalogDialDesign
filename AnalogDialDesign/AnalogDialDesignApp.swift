08176a0b6e1a6964b4fa220851cc1e2cfbd7a3ee
basker <social724@gmail.com>
2023-05-16 15:41:30 +0900

    Initial Commit

---
+++ AnalogDialDesign/AnalogDialDesignApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnalogDialDesignApp.swift
+//  AnalogDialDesign
+//
+//  Created by HYEONJUN PARK on 2023/05/16.
+//
+
+import SwiftUI
+
+@main
+struct AnalogDialDesignApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
